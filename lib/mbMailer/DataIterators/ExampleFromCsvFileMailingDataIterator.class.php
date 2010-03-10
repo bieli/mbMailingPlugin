b7ff5f2027dfb99876fa6855f6ba2c168578acc4
Marcin Bielak <marcin.bieli@gmail.com>
2010-03-10 22:55:59 +0100

    rearange sources

---
+++ lib/mbMailer/DataIterators/ExampleFromCsvFileMailingDataIterator.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class ExampleFromCsvMailingDataIterator implements mbIMailingDataIterator
+{
+    $parameters = array();
+    $propertyObj = null;
+
+//TODO: add fields for data resultsset iterations
+
+    public function init($i_Parameters)
+    {
+        $this->parameters = $i_Parameters;
+
+//TODO: connecting with database by inputed parameters
+
+        return true;
+    }
+
+    public function deinit()
+    {
+//TODO: disconnecting with database
+
+        return true;
+    }
+
+    public function setPropertiesFlags(mbMailerItemProperties $i_PropertyObj)
+    {
+
+    }
+
+    public function current()
+    {
+
+    }
+
+    public function key()
+    {
+
+    }
+
+    public function next()
+    {
+
+    }
+
+    public function rewind()
+    {
+
+    }
+
+    public function valid()
+    {
+
+    }
+}
+
---
+++ lib/mbMailer/abstracts/mbIMailerItemProperties.interface.php
@@ -0,0 +1,12 @@
+<?php
+
+//TODO: below code it is PROTOTYPE only !
+//TODO: phpdoc comments
+
+interface mbIMailerItemProperties
+{
+    public function enablePropertyFlag($i_PropertyConst);
+    public function disablePropertyFlag($i_PropertyConst);
+    public function getPropertyFlag($i_PropertyConst);
+}
+
